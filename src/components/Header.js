ac8e62823009a420c61b9e6ef41568e551cec3dc
Luke Roberts <luke@lukeroberts.me>
2017-06-25 14:46:00 +0100

    Added a Container component which accepts children

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,21 @@
 import React from 'react';
 
+// Components
+import Container from './Container';
+
+// Assets
 import logo from '../assets/logo.png';
 
 const Header = () => (
   <header className="header">
-    <div className="header__branding">
-        <img src={logo} alt="" />
-    </div>
-    <div className="header__heading">
-        <h1>{`Secure negotiation centre`}</h1>
-    </div>
+      <Container>
+          <div className="header__branding">
+              <img src={logo} alt="" />
+          </div>
+          <div className="header__heading">
+              <h1>{`Secure negotiation centre`}</h1>
+          </div>
+      </Container>
   </header>
 );
 
