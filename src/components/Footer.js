25b6b8b9388faa1888163b2e205b37cbf3a51fe7
Luke Roberts <luke@lukeroberts.me>
2017-06-25 16:31:34 +0100

    social links component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,22 @@
 import React from 'react';
 
+// Mock
+import { socialData as data } from '../mock/social-data';
+
+// Components
+import SocialLinks from './SocialLinks';
+import SocialLink from './SocialLink';
+import Container from './Container';
+
 const Footer = () => (
     <footer className="footer">
-        <p>Footer</p>
+        <Container uniqueClassName="footer__container">
+            <SocialLinks uniqueClassName="footer__socialLinks">
+                {data.map((social, index) => (
+                    <SocialLink name={social.name} url={social.url} key={index} />
+                ))}
+            </SocialLinks>
+        </Container>
     </footer>
 );
 
---
+++ src/mock/social-data.js
@@ -0,0 +1,22 @@
+export const socialData = [
+    {
+        name: 'Twitter',
+        url: 'http://twitter.com'
+    },
+    {
+        name: 'Facebook',
+        url: 'http://facebook.com',
+    },
+    {
+        name: 'Youtube',
+        url: 'http://youtube.com'
+    },
+    {
+        name: 'Pinterest',
+        url: 'http://pinterest.com'
+    },
+    {
+        name: 'googleplus',
+        url: 'http://googleplus.com'
+    }
+];
