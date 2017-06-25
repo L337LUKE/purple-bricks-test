853bc01045a09f9a8057681b500265eddbf9896f
Luke Roberts <luke@lukeroberts.me>
2017-06-25 12:59:16 +0100

    change app.css to scss and add bourbon/bourbon-neat/normalizr to the project

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import './app.css';
+import './App.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
